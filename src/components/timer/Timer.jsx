ca96b4cd04f39c7bb60c09363446f218c85315ab
Dino Nguyen, Drew Yoxsimer and Philip Eskins <example@example.com>
2019-05-13 14:08:04 -0700

    Add working 25 min timer button with state

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -7,31 +7,38 @@ class Timer extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      time: null,
+      time: 0,
     }
     this.updateTimer = this.updateTimer.bind(this);
-  }
-
-  updateTimer() {
-    let newTime = this.state.time.subtract(1, 'seconds');
-    this.setState({time: newTime})
+    // this.componentWillMount = this.componentWillMount.bind(this);
+    this.startTimer = this.startTimer.bind(this);
   }
 
   componentWillMount() {
     const setTime = new Moment.duration(25, 'minutes');
     this.setState({time: setTime});
-    this.state.time = setInterval(() =>
-      this.updateTimer(), 1000
-    );
-
   }
 
+  updateTimer() {
+    console.log(this.state.time)
+    let newTime = this.state.time.subtract(1, 'seconds');
+    this.setState({time: newTime});
+  }
 
+  startTimer() {
+    const setTime = new Moment.duration(25, 'minutes');
+    this.setState({time: setTime})
+    console.log(this.state.time)
+    setInterval(() => {
+      this.updateTimer()
+    },1000)
+  }
 
   render() {
     return(
       <div>
         <h1>Timer works</h1>
+        <button type='button' onClick={this.startTimer}>Start Focusing</button>
         {this.state.time._data.minutes} : {this.state.time._data.seconds}
       </div>
     );
