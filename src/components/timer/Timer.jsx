46c14017a022535befb8dc14d056de5c73d6337b
Dino Nguyen, Drew Yoxsimer and Philip Eskins <example@example.com>
2019-05-13 14:53:00 -0700

    Add long and short break buttons

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -8,6 +8,8 @@ class Timer extends React.Component {
     super(props)
     this.state = {
       time: 0,
+      timer: null,
+      count: 0,
     }
     this.updateTimer = this.updateTimer.bind(this);
     // this.componentWillMount = this.componentWillMount.bind(this);
@@ -20,26 +22,41 @@ class Timer extends React.Component {
   }
 
   updateTimer() {
-    console.log(this.state.time)
+    // console.log(this.state.time)
     let newTime = this.state.time.subtract(1, 'seconds');
     this.setState({time: newTime});
+    if (this.state.time._data.minutes === 0 && this.state.time._data.seconds === 0) {
+      clearInterval(this.state.timer)
+      let newCount = this.state.count + 1;
+      this.setState({count: newCount});
+      console.log(this.state.count);
+    }
   }
 
-  startTimer() {
-    const setTime = new Moment.duration(25, 'minutes');
+  displayButton() {
+    if (this.state.count % 7 === 0) {
+      longBreakButton
+    }
+  }
+
+  startTimer(number) {
+    const setTime = new Moment.duration(number, 'minutes');
     this.setState({time: setTime})
-    console.log(this.state.time)
-    setInterval(() => {
+    // console.log(this.state.time)
+    let timerStart = setInterval(() => {
       this.updateTimer()
     },1000)
+    this.setState({timer: timerStart})
   }
 
   render() {
     return(
       <div>
         <h1>Timer works</h1>
-        <button type='button' onClick={this.startTimer}>Start Focusing</button>
         {this.state.time._data.minutes} : {this.state.time._data.seconds}
+        <button className="focusButton" type='button' onClick={() => this.startTimer(25)}>Start Focusing</button>
+        <button className="shortBreakButton" type='button' onClick={() => this.startTimer(5)}>Short Break</button>
+        <button type='longBreakButton' onClick={() => this.startTimer(15)}>Long Break</button>
       </div>
     );
   }
