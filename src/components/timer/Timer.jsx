5992b7ccce4f64d865a3e8c88e89f43824865b04
Dino Nguyen, Drew Yoxsimer and Philip Eskins <dino@>
2019-05-14 11:25:45 -0700

    Add progress with formatting time

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'moment';
-
+// import MomentDurationFormat from 'moment-duration-format';
 
 class Timer extends React.Component {
   constructor(props){
